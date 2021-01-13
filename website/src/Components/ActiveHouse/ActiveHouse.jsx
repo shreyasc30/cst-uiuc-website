d1294921d25a9c9032a671563c8f8e313654828d
im-jmop <pinskyjustin@gmail.com>
2021-01-12 22:54:40 -0600

    created a constants file

--- website/src/Components/ActiveHouse/ActiveHouse.jsx
+++ website/src/Components/ActiveHouse/ActiveHouse.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import {EBOARD, ACTIVEHOUSE} from '../../constants';
 
-const ActiveHouse = ({ eboard, positions }) => {
+const ActiveHouse = () => {
 
     const eboardContent = <div className="bros">
-        {Object.entries(eboard).map(([pos, bro], i) => (
+        {Object.entries(EBOARD).map(([pos, bro], i) => (
             <div className="entry">
                 <div className='title pos'>{pos}</div>
                 <img src={bro == 'x119bolin' ? require(`../../assets/${bro}.jpg`) : require(`../../assets/sample_prof.jpg`)} alt="CST"/>
-                <div className='title bro'>{positions[bro].name}</div>
-                <div className='info'>{positions[bro].chair.map((chair, i) => i > 0 ? `, ${chair}` : chair)}</div>
-                <div className='info'>{positions[bro].year}</div>
-                <div className='info'>{positions[bro].crossed}</div>
+                <div className='title bro'>{ACTIVEHOUSE[bro].name}</div>
+                <div className='info'>{ACTIVEHOUSE[bro].chair.map((chair, i) => i > 0 ? `, ${chair}` : chair)}</div>
+                <div className='info'>{ACTIVEHOUSE[bro].year}</div>
+                <div className='info'>{ACTIVEHOUSE[bro].crossed}</div>
             </div>
         ))}
     </div>
 
     const cabinet = <div className="bros">
-        {Object.entries(positions).map(([bro, {name, chair, year, crossed}], i) => (
-            !(Object.values(eboard).includes(bro)) &&
+        {Object.entries(ACTIVEHOUSE).map(([bro, {name, chair, year, crossed}], i) => (
+            !(Object.values(EBOARD).includes(bro)) &&
             <div className="entry">
                 <img src={require('../../assets/sample_prof.jpg')} alt="CST"/>
                 <div className='title bro'>{name}</div>
---
+++ website/src/constants.js
@@ -0,0 +1,71 @@
+export const EBOARD = {
+    'President': 'x116phantom',
+    'Vice President Internal': 'x129wilt',
+    'Vice President External': 'x117tempo',
+    'Treasurer': 'x119bolin',
+    'Secretary': 'x127rasen',
+    'Judicial': 'x114caveman',
+}
+
+export const ACTIVEHOUSE = {
+    'x112intro': {
+        'name': 'x112 Yun Mo "Intro" Yang',
+        'chair': ['Chapter Advisor'],
+        'year': 'Senior',
+        'crossed': 'rhoguys (fa17)',
+    },
+    'x114caveman': {
+        'name': 'x114 Justin "Caveman" Arcilla',
+        'chair': ['Chapter Advisor','extra'],
+        'year': 'Senior',
+        'crossed': 'rhoguys (fa17)',
+    },
+    'x116phantom': {
+        'name': 'x116 Tristan "Phantom" Empalmado',
+        'chair': ['Chapter Advisor'],
+        'year': 'Senior',
+        'crossed': 'rhoguys (fa17)',
+    },
+    'x117mustang': {
+        'name': 'x117 Andre "Mustang" Castillo',
+        'chair': ['Chapter Advisor'],
+        'year': 'Senior',
+        'crossed': 'rhoguys (fa17)',
+    },
+    'x117tempo': {
+        'name': 'x117 Koji "Tempo" Wong',
+        'chair': ['Chapter Advisor'],
+        'year': 'Senior',
+        'crossed': 'rhoguys (fa17)',
+    },
+    'x119bolin': {
+        'name': 'x119 Seongsik "Bolin" Yang',
+        'chair': ['Chapter Advisor'],
+        'year': 'Senior',
+        'crossed': 'rhoguys (fa17)',
+    },
+    'x122spirit': {
+        'name': 'x122 Kurtis "Spirit" Dinh',
+        'chair': ['Chapter Advisor'],
+        'year': 'Senior',
+        'crossed': 'rhoguys (fa17)',
+    },
+    'x126quiv': {
+        'name': 'x126 Jason "Quiv" Abe',
+        'chair': ['Chapter Advisor'],
+        'year': 'Senior',
+        'crossed': 'rhoguys (fa17)',
+    },
+    'x127rasen': {
+        'name': 'x127 Mark Andre "Rasen" Adalin',
+        'chair': ['Chapter Advisor','extra', 'more'],
+        'year': 'Senior',
+        'crossed': 'rhoguys (fa17)',
+    },
+    'x129wilt': {
+        'name': 'x129 Shreyas "Wilt" Chandrashekaran',
+        'chair': ['Chapter Advisor','extra', 'more'],
+        'year': 'Senior',
+        'crossed': 'rhoguys (fa17)',
+    },
+}
