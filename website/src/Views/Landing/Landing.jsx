eea3029ba42efd63b86ce333b08434cf05cd61ea
FIRST_NAME LAST_NAME <mineyev2@illinois.edu>
2022-01-03 15:44:18 -0600

    Made Leaders not followers variable to the size of display, and made background image not clp on mobile version

--- website/src/Views/Landing/Landing.jsx
+++ website/src/Views/Landing/Landing.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+
+// to make it mobile-friendly
+import MediaQuery from 'react-responsive';
+
 import Navigation from '../../Components/Navigation/Navigation';
 import Footer from '../../Components/Footer/Footer';
 
@@ -6,9 +10,39 @@ function Landing() {
     return (
         <>
             <Navigation />
+            {/* 
+            <div>
+
+                <h1>Device Test!</h1>
+                <MediaQuery minWidth={1224}>
+                    <p>You are a desktop or laptop</p>
+                <MediaQuery minWidth={1824}>
+                    <p>You also have a huge screen</p>
+                </MediaQuery>
+                </MediaQuery>
+                <MediaQuery minResolution="2dppx">
+                {(matches) =>
+                    matches
+                    ? <p>You are retina</p>
+                    : <p>You are not retina</p>
+                }
+                </MediaQuery>
+                
+            </div>
+            */}
+
             <div className="Landing">
                 <div>
-                    <h1 className="landing-text">Leaders Not Followers.</h1>
+                    {/* don't this this is the best way to go about it but it's whatever*/}
+                    <MediaQuery minWidth={500}>
+                    {(matches) =>
+                        matches
+                        ? <h1 className="landing-text-computer">Leaders Not Followers.</h1>
+                        : <h1 className="landing-text-mobile">Leaders Not Followers.</h1>
+                    }
+                    </MediaQuery>
+
+
                     <img src={require('../../assets/sample_pic.jpg')} alt="CST"/>
                 </div>
             </div>
