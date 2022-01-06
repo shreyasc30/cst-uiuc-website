4c2875552dc38945c7c05823f110d95183c43c9f
FIRST_NAME LAST_NAME <mineyev2@illinois.edu>
2022-01-06 16:23:28 -0600

    background fixed, website is mobile-friendlygit add -A

--- website/src/Views/Landing/Landing.jsx
+++ website/src/Views/Landing/Landing.jsx
@@ -1,47 +1,27 @@
 import React from 'react';
 
-// to make it mobile-friendly
-import MediaQuery from 'react-responsive';
-
 import Navigation from '../../Components/Navigation/Navigation';
 import Footer from '../../Components/Footer/Footer';
 
 function Landing() {
     return (
-        <>
-            <Navigation />
-            {/* 
-            <div>
+        <>  
+            <div className="background">
 
-                <h1>Device Test!</h1>
-                <MediaQuery minWidth={1224}>
-                    <p>You are a desktop or laptop</p>
-                <MediaQuery minWidth={1824}>
-                    <p>You also have a huge screen</p>
-                </MediaQuery>
-                </MediaQuery>
-                <MediaQuery minResolution="2dppx">
-                {(matches) =>
-                    matches
-                    ? <p>You are retina</p>
-                    : <p>You are not retina</p>
-                }
-                </MediaQuery>
-                
-            </div>
-            */}
+                <Navigation />
 
-            <div className="Landing">
-                <div>
-                    {/* don't this this is the best way to go about it but it's whatever*/}
-                    <h1 className="landing-text">Leaders Not Followers</h1>
-
-                    <img src={require('../../assets/ABCCrossing.jpg')} alt="CST"/>
+                <div className="Landing">
+                    <div>
+                        <h1 className="landing-text">Leaders Not Followers</h1>
+                    </div>
                 </div>
+
+                <div className="bottom">
+                <Footer className="footer"/>
+                </div>
+            
             </div>
-            <div className="bottom">
-            <Footer className="footer"/>
-            </div>
+
         </>
     );
 }
